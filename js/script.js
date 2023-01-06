8be6beefc7b9233929695041ef59796ef0a28ee0
davidGalaviz <110849310+davidGalaviz@users.noreply.github.com>
2023-01-06 11:09:36 -0600

    mostrar disponibilidad de tamaños

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $(document).ready(function () {
   const labelNombre = $("label[for='nombre']");
   const inputApellidos = $("#apellidos");
   const labelApellidos = $("label[for='apellidos']");
+  const tamanoSelect = $("#tamano");
+  const resultadoDisponibilidadTamano = $("#resultado_tamano");
+
+  tamanoSelect.on("change", function() {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:5000/checksize",
+      data: {
+        size: this.value
+      },
+      success:function(result) {
+        resultadoDisponibilidadTamano.text(result);
+      }
+    })
+  })
 
   formulario.submit(function (evt) {
     if (inputNombre.val() === "" || !inputNombre.val()) {
